cbc4dabaa7cd2f9a32a22851b7b9c8bbf4e3df9d
Jackson Westeen <jcw21@email.arizona.edu>
2015-03-01 16:14:48 -0500

    put together trip updates and vehicles

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = http.createServer(handler),
 //app.listen(8080);
 
 
-function handler (req, res) {
+function handler(req, res) {
     fs.readFile(__dirname + '/index.html',
         function (err, data) {
             if (err) {
@@ -39,10 +39,10 @@ function parseRoutes() {
     var deferred = q.defer(),
         whitelist = "Red Line, Blue Line, Green Line, Orange Line".split(', ');
 
-    csv.parse(fs.readFileSync('mbta_gtfs/routes.txt'), function(err, data) {
+    csv.parse(fs.readFileSync('mbta_gtfs/routes.txt'), function (err, data) {
         if (err) deferred.reject(err);
         else {
-            deferred.resolve(_.reduce(data, function(obj, arr) {
+            deferred.resolve(_.reduce(data, function (obj, arr) {
                 _(whitelist).contains(arr[3]) && (obj[arr[0]] = arr[3]);
                 return obj;
             }, {}));
@@ -56,64 +56,61 @@ function parseStations() {
     var deferred = q.defer(),
         whitelist = ['1'];
 
-    csv.parse(fs.readFileSync('mbta_gtfs/stops.txt'), function(err, data) {
+    csv.parse(fs.readFileSync('mbta_gtfs/stops.txt'), function (err, data) {
         if (err) deferred.reject(err);
         else {
-            deferred.resolve(_.reduce(data, function(obj, arr) {
+            deferred.resolve(_.reduce(data, function (obj, arr) {
                 _(whitelist).contains(arr[8]) && (obj[arr[0]] = arr[2]);
                 return obj;
             }, {}));
         }
     });
 
-    return deferred.pgromise;
+    return deferred.promise;
 }
 
-parseStations()
-    .then(function(result) {
-        debugger;
+function fetchFeed(url, parser) {
+    var transit = ProtoBuf.protoFromFile('gtfs-realtime.proto').build('transit_realtime');
+    // ^^ create a protobuf decoder // your protobuf binary feed URL
+
+    http.get(url, function (res) {
+        var data = [];
+        res.on("data", function (chunk) {
+            data.push(chunk);
+        });
+        res.on("end", function () {
+            // create a FeedMessage object by decooding the data with the protobuf object
+            parser(transit.FeedMessage.decode(Buffer.concat(data)))
+        });
     });
+}
 
-// create a protobuf decoder // your protobuf binary feed URL
-var transit = ProtoBuf.protoFromFile('gtfs-realtime.proto').build('transit_realtime');
+parseRoutes()
+    .then(function (routes) {
+        fetchFeed('http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb',
+            function (msg) {
+                var vehicles = _(msg.entity)
+                    .pluck('vehicle')
+                    .filter(function (vehicle) {
+                        return _.contains(_.keys(routes), vehicle.trip.route_id);
+                    })
+                    .groupBy(function(vehicle) {
+                        return vehicle.vehicle.id
+                    }).value();
 
+                fetchFeed('http://developer.mbta.com/lib/GTRTFS/Alerts/TripUpdates.pb',
+                    function(msg) {
+                        var updates = _.filter(msg.entity, function(entity) {
+                            return _.contains(_.keys(routes), entity.trip_update.trip.route_id);
+                        });
 
-// get trip progress and updates
-http.get('http://developer.mbta.com/lib/GTRTFS/Alerts/TripUpdates.pb', function(res) {
-     var data = [];
-    res.on("data", function(chunk) { data.push(chunk); });
-    res.on("end", function() {
-        data = Buffer.concat(data);
+                        _.forEach(updates, function(update) {
+                            debugger;
+                            update.trip_update.vehicle = vehicles[update.trip_update.vehicle.id];
+                        });
 
-        // create a FeedMessage object by decooding the data with the protobuf object
-        var msg = transit.FeedMessage.decode(data);
-
-            //vehicles = _.pluck(msg.entity, 'vehicle');
-
-
-
-
-        //console.log(JSON.stringify(msg, null, 2));
-        //debugger;
-    });    
-
-});
-
-// get vehicle positions
-http.get('http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb', function(res) {
-    var data = [];
-    res.on("data", function(chunk) { data.push(chunk); });
-    res.on("end", function() {
-        data = Buffer.concat(data);
-
-        // create a FeedMessage object by decooding the data with the protobuf object
-        var msg = transit.FeedMessage.decode(data),
-            vehicles = _.pluck(msg.entity, 'vehicle');
-
-
-
-        //console.log(JSON.stringify(vehicles, null, 2));
-        //debugger;
+                        console.log(JSON.stringify(updates, null, 2));
+                    });
+            }
+        );
     });
-});
-
