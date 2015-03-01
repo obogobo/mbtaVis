b505a4ad244d36d35c3e98f47a2aac47361e2367
Jackson Westeen <jcw21@email.arizona.edu>
2015-03-01 15:10:12 -0500

    parsing human readable data from csv w/ promises

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http'),
     ProtoBuf = require('protobufjs'),
+    csv = require('csv'),
     q = require('q'),
     _ = require('lodash');
 
@@ -8,7 +9,7 @@ var app = http.createServer(handler),
     fs = require('fs');
 
 // start the server
-app.listen(8080);
+//app.listen(8080);
 
 
 function handler (req, res) {
@@ -33,6 +34,46 @@ io.on('connection', function (socket) {
 });
 
 
+// maps route_ids -> route_names
+function parseRoutes() {
+    var deferred = q.defer(),
+        whitelist = "Red Line, Blue Line, Green Line, Orange Line".split(', ');
+
+    csv.parse(fs.readFileSync('mbta_gtfs/routes.txt'), function(err, data) {
+        if (err) deferred.reject(err);
+        else {
+            deferred.resolve(_.reduce(data, function(obj, arr) {
+                _(whitelist).contains(arr[3]) && (obj[arr[0]] = arr[3]);
+                return obj;
+            }, {}));
+        }
+    });
+
+    return deferred.promise;
+}
+
+function parseStations() {
+    var deferred = q.defer(),
+        whitelist = ['1'];
+
+    csv.parse(fs.readFileSync('mbta_gtfs/stops.txt'), function(err, data) {
+        if (err) deferred.reject(err);
+        else {
+            deferred.resolve(_.reduce(data, function(obj, arr) {
+                _(whitelist).contains(arr[8]) && (obj[arr[0]] = arr[2]);
+                return obj;
+            }, {}));
+        }
+    });
+
+    return deferred.pgromise;
+}
+
+parseStations()
+    .then(function(result) {
+        debugger;
+    });
+
 // create a protobuf decoder // your protobuf binary feed URL
 var transit = ProtoBuf.protoFromFile('gtfs-realtime.proto').build('transit_realtime');
 
@@ -52,8 +93,8 @@ http.get('http://developer.mbta.com/lib/GTRTFS/Alerts/TripUpdates.pb', function(
 
 
 
-        console.log(JSON.stringify(msg, null, 2));
-        debugger;
+        //console.log(JSON.stringify(msg, null, 2));
+        //debugger;
     });    
 
 });
@@ -72,7 +113,7 @@ http.get('http://developer.mbta.com/lib/GTRTFS/Alerts/VehiclePositions.pb', func
 
 
         //console.log(JSON.stringify(vehicles, null, 2));
-        debugger;
+        //debugger;
     });
 });
 
